3efe31ca677d219e39b4860304c502b6fafc44f5
TheoWinter <theo.winter@gmail.com>
2014-02-26 17:40:25 +0100

    Made the server run indefinitly so that we can keep sending fractal requests.

--- VSSProgExercises/srcEx02/Ex02_5_Fraktal/Ex02_5_FraktalServer.java
+++ VSSProgExercises/srcEx02/Ex02_5_Fraktal/Ex02_5_FraktalServer.java
@@ -13,28 +13,30 @@ public class Ex02_5_FraktalServer {
 	public static final int PORT = 16666;
 
 	public static void main(String[] args) throws Exception {
-        try (ServerSocket ss = new ServerSocket(PORT)) {
-        	System.out.println("waiting on client...");
-        	Socket s = ss.accept();  
-        	InputStream is = s.getInputStream();  
-        	ObjectInputStream ois = new ObjectInputStream(is);
-        	Ex02_5_FraktalHelper fractalHelp = (Ex02_5_FraktalHelper)ois.readObject();
-        	
-        	Ex02_5_FraktalGenerator generator = new Ex02_5_FraktalGenerator();
-        	ImageIcon result = generator.calcFraktal(fractalHelp.xMin, fractalHelp.xMax, fractalHelp.yMin, fractalHelp.yMax, fractalHelp.width, fractalHelp.maxDeep);
-        	
-        	System.out.println("Successfully received fractal request, sending fractal now.");
-        	
-	    	OutputStream os = s.getOutputStream();  
-	    	ObjectOutputStream oos = new ObjectOutputStream(os);  
-	    	
-	    	oos.writeObject(result);
-	    	oos.close();  
-	    	os.close();  
-
-        	is.close();  
-        	s.close();  
-        	ss.close();
+		while(true){
+	        try (ServerSocket ss = new ServerSocket(PORT)) {
+	        	System.out.println("waiting on client...");
+	        	Socket s = ss.accept();  
+	        	InputStream is = s.getInputStream();  
+	        	ObjectInputStream ois = new ObjectInputStream(is);
+	        	Ex02_5_FraktalHelper fractalHelp = (Ex02_5_FraktalHelper)ois.readObject();
+	        	
+	        	Ex02_5_FraktalGenerator generator = new Ex02_5_FraktalGenerator();
+	        	ImageIcon result = generator.calcFraktal(fractalHelp.xMin, fractalHelp.xMax, fractalHelp.yMin, fractalHelp.yMax, fractalHelp.width, fractalHelp.maxDeep);
+	        	
+	        	System.out.println("Successfully received fractal request, sending fractal now.");
+	        	
+		    	OutputStream os = s.getOutputStream();  
+		    	ObjectOutputStream oos = new ObjectOutputStream(os);  
+		    	
+		    	oos.writeObject(result);
+		    	oos.close();  
+		    	os.close();  
+	
+	        	is.close();  
+	        	s.close();  
+	        	ss.close();
+	        }
         }
     }
 }
